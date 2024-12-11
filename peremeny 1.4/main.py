a0909d6d25eb5487cd1738b2c909e6caa20e6b18
PCSpro <drago110103@gmail.com>
2024-12-11 14:34:07 +0500

    Add files via upload

---
+++ peremeny 1.4/main.py
@@ -0,0 +1,5 @@
+number_of_completed_tasks = 12
+number_of_hours = 1.5
+course = "Python"
+time_for_one_task = number_of_completed_tasks/number_of_hours
+print('Курс:', course, ', всего задач:', number_of_completed_tasks, ', затрачено часов:', number_of_hours,', среднее время выполнения', time_for_one_task)
